f33ab69887cf608c813ee3026db750cacffa171e
KassioVieira <kassio.vieira7@gmail.com>
2020-03-15 10:59:37 -0300

    final version app with vue js, that version have build to production

--- src/App.vue
+++ src/App.vue
@@ -8,92 +8,7 @@
       <h6 class="text-secondary">Generate names with VueJs, GraphQl and Node</h6>
     </div>
     <div id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md">
-            <h5>
-              Prefixos
-              <span class="badge badge-info">{{prefixes.length}}</span>
-            </h5>
-            <div class="card-body">
-              <ul class="list-group">
-                <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-                  <div class="row">
-                    <div class="col-md">{{prefix}}</div>
-                    <div class="col-md text-right">
-                      <button class="btn btn-info" v-on:click="deletePrefix(prefix)">
-                        <span class="fa fa-trash"></span>
-                      </button>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-              <br />
-              <div class="input-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  v-model="prefix"
-                  v-on:keyup.enter="addPrefix(prefix)"
-                  placeholder="Digite aqui o prefixo"
-                />
-                <div class="input-group-append">
-                  <button class="btn btn-info" v-on:click="addPrefix(prefix)">
-                    <span class="fa fa-plus"></span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md">
-            <h5>
-              Sufixos
-              <span class="badge badge-info">{{sufixes.length}}</span>
-            </h5>
-            <div class="card-body">
-              <ul class="list-group">
-                <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
-                  <div class="row">
-                    <div class="col-md">{{sufix}}</div>
-                    <div class="col-md text-right">
-                      <button class="btn btn-info" v-on:click="deleteSufix(sufix)">
-                        <span class="fa fa-trash"></span>
-                      </button>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-              <br />
-              <div class="input-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  v-model="sufix"
-                  v-on:keyup.enter="addSufix(sufix)"
-                  placeholder="Digite aqui o sufixo"
-                />
-                <div class="input-group-append">
-                  <button class="btn btn-info" v-on:click="addSufix(sufix)">
-                    <span class="fa fa-plus"></span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <br />
-        <h5>
-          Domains
-          <span class="badge badge-info">{{domains.length}}</span>
-        </h5>
-        <div class="card">
-          <div class="card-body">
-            <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">{{domain}}</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <DomainList></DomainList>
     </div>
   </div>
 </template>
@@ -101,56 +16,12 @@
 <script>
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import DomainList from "./components/DomainList";
 
 export default {
   name: "App",
-  data: function() {
-    return {
-      prefix: "",
-      sufix: "",
-      prefixes: ["Air", "Jet", "Flight"],
-      sufixes: ["Hub", "Station", "Mart"],
-      domains: [
-        "AirHub",
-        "AirStation",
-        "AirMart",
-        "JetHub",
-        "JetStation",
-        "JetMart",
-        "FlightHub",
-        "FlightStation",
-        "FlightMart"
-      ]
-    };
-  },
-
-  methods: {
-    addPrefix(prefix) {
-      this.prefixes.push(prefix);
-      this.prefix = "";
-      this.generate();
-    },
-    deletePrefix(prefix) {
-      this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
-      this.generate();
-    },
-    addSufix(sufix) {
-      this.sufixes.push(sufix);
-      this.sufix = "";
-      this.generate();
-    },
-    deleteSufix(sufix) {
-      this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
-      this.generate();
-    },
-    generate() {
-      this.domains = [];
-      for (const prefix of this.prefixes) {
-        for (const sufix of this.sufixes) {
-          this.domains.push(prefix + sufix);
-        }
-      }
-    }
+  components: {
+    DomainList
   }
 };
 </script>
@@ -160,7 +31,6 @@ export default {
   margin-top: 30px;
   margin-bottom: 30px;
 }
-
 #main {
   background-color: #f1f1f1;
   margin-top: 30px;
